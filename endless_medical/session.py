3043ad5bc9a39c3fe3fd62aaf60b3cb938a566d2
_Leg3ndary <ben.zhou.2006@gmail.com>
2023-01-07 17:48:36 -0500

    Add accepttos stuff

--- endless_medical/session.py
+++ endless_medical/session.py
@@ -3,6 +3,7 @@ Session information
 """
 
 import asyncio
+import aiohttp
 from typing import Dict
 
 
@@ -13,6 +14,7 @@ class Session:
 
     def __init__(
         self,
+        sesh: aiohttp.ClientSession,
         loop: asyncio.BaseEventLoop,
         session_id: str,
         default: bool,
@@ -27,13 +29,32 @@ class Session:
         if auto_accept:
             loop.run_in_executor(None, self.accept_terms_of_use)
 
+    async def request(self, method: str, url: str, **kwargs) -> Dict:
+        """
+        Make a request to the API
+        """
+        async with self.sesh.request(
+            method, f"https://api.endlessmedical.com/v1/dx{url}", **kwargs
+        ) as response:
+            data = await response.json()
+            return data
+
     async def accept_terms_of_use(self) -> None:
         """
         Accept the terms of use
         """
         data = await self.request(
             "POST",
-            f"/AcceptTermsOfUse?SessionID={self.session_id}&passphrase=I have read, understood and I accept and agree to comply with the Terms of Use of EndlessMedicalAPI and Endless Medical services. The Terms of Use are available on endlessmedical.com",
+            "/AcceptTermsOfUse",
+            params={
+                "SessionID": self.session_id,
+                "passphrase": "I have read, understood and I accept and agree to comply with the Terms of Use of EndlessMedicalAPI and Endless Medical services. The Terms of Use are available on endlessmedical.com"
+            },
         )
         if data["status"] != "ok":
             raise Exception("Failed to accept terms of use")
+
+    async def add_feature(self, feature: str) -> None:
+        """
+        Add a feature
+        """
